fe034b69e6fc510c6fed452109e5e690e86aedcf
Catherine Cordova <ccordova41@gmail.com>
2017-11-15 18:24:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(s) {
+    return document.querySelector(s)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let i = 0, l = children.length; i < l; i++) {
+      children[i].innerHTML = parseInt(children[i].innerHTML) + n
+    }
+  }
+}
+
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
